62c93b418dc5172b63b6bf7ea34090d0748a428e
Stephen Booth <me@sbooth.org>
2026-01-02 17:46:44 -0600

    Add SPDX tags (#1)

--- Sources/CXXSharedMutex/include/CXXSharedMutex/SharedMutex.hpp
+++ Sources/CXXSharedMutex/include/CXXSharedMutex/SharedMutex.hpp
@@ -1,7 +1,8 @@
 //
-// Copyright © 2025 Stephen F. Booth
+// SPDX-FileCopyrightText: 2025 Stephen F. Booth <contact@sbooth.dev>
+// SPDX-License-Identifier: MIT
+//
 // Part of https://github.com/sbooth/CXXSharedMutex
-// MIT license
 //
 
 #pragma once
--- Tests/CXXSharedMutexTests/CXXSharedMutexTests.swift
+++ Tests/CXXSharedMutexTests/CXXSharedMutexTests.swift
@@ -1,3 +1,10 @@
+//
+// SPDX-FileCopyrightText: 2025 Stephen F. Booth <contact@sbooth.dev>
+// SPDX-License-Identifier: MIT
+//
+// Part of https://github.com/sbooth/CXXSharedMutex
+//
+
 import Testing
 import Foundation
 @testable import CXXSharedMutex
