e2473b9f8d81064bab3a5ca5fb071f491d135ae3
Stephen Booth <me@sbooth.org>
2026-01-02 17:45:12 -0600

    Add SPDX tags

--- Sources/CXXSharedMutex/SharedMutex.cpp
+++ Sources/CXXSharedMutex/SharedMutex.cpp
@@ -1,5 +1,6 @@
 //
-// Copyright © 2025 Stephen F. Booth
+// SPDX-FileCopyrightText: 2025 Stephen F. Booth <contact@sbooth.dev>
+// SPDX-License-Identifier: MIT
+//
 // Part of https://github.com/sbooth/CXXSharedMutex
-// MIT license
 //
